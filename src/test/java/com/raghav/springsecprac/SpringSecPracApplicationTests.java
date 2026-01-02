951e6aaa6e0849c9cc073f5733736451afcb5889
Raghav893 <raghavarora2309@gmail.com>
2026-01-02 23:33:33 +0530

    Created a project to practice spring security in it

---
+++ src/test/java/com/raghav/springsecprac/SpringSecPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.springsecprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
